5c8bda4e211aafd0ed29dc829c0d4b301c2e0899
Timothy Hoang <timh1203@gmail.com>
2017-09-19 19:47:54 -0400

    Add pseudocode for js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Get latitude and longitude
+
+    // API call to Dark Sky with coordinates
+
+    // Retrieve and render weather data
+
+    // Show Skycons with weather conditions
+
+    // Unit conversion button
+
+}) // End doc ready
