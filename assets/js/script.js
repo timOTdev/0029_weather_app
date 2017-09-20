86ad36595e2dfba58b789b1f0738a095a7ba3f20
Timothy Hoang <timh1203@gmail.com>
2017-09-19 20:27:54 -0400

    Add ipinfo.io to get location information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
-    // Get latitude and longitude
+    // Get city, state, latitude, and longitude
+    // Form query string to send to Dark Sky
+    $.getJSON("https://ipinfo.io/", function(response) {
+        $("#location").html(response.city + ", " + response.region);
+        var corsURL = "https://cors-anywhere.herokuapp.com/";
+        var darkSkyKey = "1fdf13244735936bf1a2b5d6de49d125";
+        var darkSkyAPI = "https://api.darksky.net/forecast/";
+        var latitude = response.loc.split(",")[0];
+        var longitude = response.loc.split(",")[1];
+        var weatherURL = corsURL + darkSkyKey + darkSkyAPI + "/" + latitude + "," + longitude;
+    })
 
-    // API call to Dark Sky with coordinates
+    // Make call to Dark Sky API
 
     // Retrieve and render weather data
-
-    // Show Skycons with weather conditions
-
+    
     // Unit conversion button
 
 }) // End doc ready
