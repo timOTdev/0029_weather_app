f8a4aeaa2aabee481d2516d9bbaaf29da357b2c6
Timothy Hoang <timh1203@gmail.com>
2017-09-22 20:45:01 -0400

    Add unit conversion button

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,62 +12,73 @@ $(document).ready(function() {
 
         // Make call to Dark Sky API
         function getData() {        
-            // Retrieve and render weather data
+            // Render weather data
             $.getJSON(weatherURL, function(data) {
-            $("#conditions").html(data.currently.summary);
-    
-            var tempF = Math.round(data.currently.temperature);
-            $("#tempF").html("Current Temperature: <br/>" + tempF + "\xB0F");
-    
-            var icon = data.currently.icon;
-            $("#icon").html("Icon: <br/>");
-    
-            var windSpeed = data.currently.windSpeed;
-            $("#windSpeed").html("Current Wind Speed: <br/>" + windSpeed + " mph");
-    
-            var precipProbability = data.currently.precipProbability;
-            $("#precipProbability").html("Chance of Precipitation: <br/>" + precipProbability + "%");
-            
-            var humidity = data.currently.humidity;
-            $("#humidity").html("Humidity: <br/>" + humidity + "%");
-            console.log(data.currently.summary);
+              $("#conditions").html(data.currently.summary);
+      
+              var tempF = Math.round(data.currently.temperature);
+              $("#tempF").html("Current Temperature: <br/>" + tempF + "\xB0F");
+      
+              var icon = data.currently.icon;
+              $("#icon").html("Icon: <br/>");
+      
+              var windSpeed = data.currently.windSpeed;
+              $("#windSpeed").html("Current Wind Speed: <br/>" + windSpeed + " mph");
+      
+              var precipProbability = data.currently.precipProbability;
+              $("#precipProbability").html("Chance of Precipitation: <br/>" + precipProbability + "%");
+              
+              var humidity = data.currently.humidity;
+              $("#humidity").html("Humidity: <br/>" + humidity + "%");
+              console.log(data.currently.summary);
 
-            var skycons = new Skycons({"color": "orange"});
-                  function weatherIcon(){
-                    if (icon === "clear-day") {
-                      skycons.add("icon1", Skycons.CLEAR_DAY);
-                    } else if (icon === "clear-night") {
-                      skycons.add("icon1", Skycons.CLEAR_NIGHT);
-                    } else if (icon === "rain") {
-                      skycons.add("icon1", Skycons.RAIN);
-                    } else if (icon === "snow") {
-                      skycons.add("icon1", Skycons.SNOW);
-                    } else if (icon === "sleet") {
-                      skycons.add("icon1", Skycons.SLEET);
-                    } else if (icon === "wind") {
-                      skycons.add("icon1", Skycons.WIND);
-                    } else if (icon === "fog") {
-                      skycons.add("icon1", Skycons.FOG);
-                    } else if (icon === "cloudy") {
-                      skycons.add("icon1", Skycons.CLOUDY);
-                    } else if (icon === "partly-cloudy-day") {
-                      skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
-                    } else if (icon === "partly-cloudy-night") {
-                      skycons.add("icon1", Skycons.PARTLY_CLOUDY_NIGHT);
-                    } else {
-                      console.log("Dark Sky icon did not return a matching case");
-                    }
-                    skycons.play();
+              // Render weather icon
+              var skycons = new Skycons({"color": "orange"});
+              function weatherIcon(){
+                if (icon === "clear-day") {
+                  skycons.add("icon1", Skycons.CLEAR_DAY);
+                } else if (icon === "clear-night") {
+                  skycons.add("icon1", Skycons.CLEAR_NIGHT);
+                } else if (icon === "rain") {
+                  skycons.add("icon1", Skycons.RAIN);
+                } else if (icon === "snow") {
+                  skycons.add("icon1", Skycons.SNOW);
+                } else if (icon === "sleet") {
+                  skycons.add("icon1", Skycons.SLEET);
+                } else if (icon === "wind") {
+                  skycons.add("icon1", Skycons.WIND);
+                } else if (icon === "fog") {
+                  skycons.add("icon1", Skycons.FOG);
+                } else if (icon === "cloudy") {
+                  skycons.add("icon1", Skycons.CLOUDY);
+                } else if (icon === "partly-cloudy-day") {
+                  skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
+                } else if (icon === "partly-cloudy-night") {
+                  skycons.add("icon1", Skycons.PARTLY_CLOUDY_NIGHT);
+                } else {
+                  console.log("Dark Sky icon did not return a matching case");
+                }
+                skycons.play();
+                }
+                weatherIcon();
+              
+              // Unit conversion button
+              function tempConversion() {
+                var tempC = Math.round((tempF - 32) * (5/9));
+                var windKPH = Math.round(windSpeed * 1.60934);
+                $("#convert").click(function() {
+                  if ($("#tempF").is(":contains('\xB0F')")) {
+                    $("#tempF").html("Current Temperature: <br/>" + tempC + "\xB0C");
+                    $("#windSpeed").html("Current Wind Speed: <br/>" + windKPH + " kph");
+                  } else {
+                    $("#tempF").html("Current Temperature: <br/>" + tempF + "\xB0F");
+                    $("#windSpeed").html("Current Wind Speed: <br/>" + windSpeed + " mph");
                   }
-                  weatherIcon();
-            })
-        }
+                });
+              }
+              tempConversion();
+              })
+        } // End getData function
         getData();
     }) // End json call
-    
-
-
-    
-    // Unit conversion button
-
 }) // End doc ready
